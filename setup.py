cd3c549fc8f5e05e43c36520470b43d1a7b4bd9f
Dmytro Fedoriaka <fedimser@gmail.com>
2025-04-18 12:44:38 -0700

    Add data to package

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 import setuptools
 
-with open("README.md", "r", encoding='utf-8') as readme_file:
+with open("README.md", "r", encoding="utf-8") as readme_file:
     long_description = readme_file.read()
 
-with open('requirements.txt', 'r', encoding='utf-8') as req_file:
+with open("requirements.txt", "r", encoding="utf-8") as req_file:
     requirements = [r.strip() for r in req_file.readlines()]
 
 setuptools.setup(
@@ -16,6 +16,10 @@ setuptools.setup(
     license="MIT",
     url="https://github.com/iKolt/cayleypy",
     packages=setuptools.find_packages(),
-    python_requires='>=3.10',
+    include_package_data=True,
+    package_data={
+        "cayleypy": ["data/*.csv"],
+    },
+    python_requires=">=3.10",
     install_requires=requirements,
 )
