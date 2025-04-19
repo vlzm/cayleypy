258d37ae9fb75e5fd1263ad75867ed8cc641b416
Dmytro Fedoriaka <fedimser@gmail.com>
2025-04-19 15:25:31 -0700

    do not install requirements in setup

--- setup.py
+++ setup.py
@@ -4,7 +4,9 @@ with open("README.md", "r", encoding="utf-8") as readme_file:
     long_description = readme_file.read()
 
 with open("requirements.txt", "r", encoding="utf-8") as req_file:
-    requirements = [r.strip() for r in req_file.readlines()]
+    # Temporarily remove requirements for faster development iteration on Kaggle.
+    # Kaggle has torch, but for some reason if it's here, it spends a lot of time installing it.
+    requirements = [] #[r.strip() for r in req_file.readlines()]
 
 setuptools.setup(
     name="cayleypy",
