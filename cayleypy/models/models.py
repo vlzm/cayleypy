6b0570abe34b9ae90bb28726f706d362e2408067
Dmytro Fedoriaka <fedimser@gmail.com>
2025-07-21 21:38:23 -0700

    add loading model from kagglehub

--- cayleypy/models/models.py
+++ cayleypy/models/models.py
@@ -1,9 +1,10 @@
-import torch
-from attr import dataclass
-from numba.core.ccallback import CFunc
-from torch import nn
-
+import os.path
 from dataclasses import dataclass
+from typing import Optional
+
+import kagglehub
+import torch
+from torch import nn
 
 
 @dataclass(frozen=True)
@@ -14,6 +15,8 @@ class ModelConfig:
     input_size: int
     num_classes_for_one_hot: int
     layers_sizes: list[int]
+    weights_kaggle_id: Optional[str]
+    weights_path: Optional[str]
 
     @staticmethod
     def from_dict(cfg: dict[str, any]):
@@ -23,14 +26,26 @@ class ModelConfig:
             input_size=cfg["input_size"],
             num_classes_for_one_hot=cfg["num_classes_for_one_hot"],
             layers_sizes=cfg["layers_sizes"],
+            weights_kaggle_id=cfg.get("weights_kaggle_id", None),
+            weights_path=cfg.get("weights_path", None),
         )
 
-    def create_model(self) -> nn.Module:
-        """Creates model described by this config."""
+    def _build_model(self) -> nn.Module:
         if self.model_type == "MLP":
             return MlpModel(self)
         else:
             raise ValueError("Unknown model type: " + self.model_type)
+
+    def load(self, device="cpu") -> nn.Module:
+        """Creates model described by this config and loads weights."""
+        model = self._build_model()
+        if self.weights_path is not None:
+            path = self.weights_path
+            if self.weights_kaggle_id is not None:
+                model_dir = kagglehub.model_download(self.weights_kaggle_id)
+                path = os.path.join(model_dir, path)
+            model.load_state_dict(torch.load(path, map_location=device))
+        return model
 
 
 class MlpModel(nn.Module):
