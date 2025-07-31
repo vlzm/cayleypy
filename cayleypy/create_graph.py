18dc9ab30ce9e3fc8c89529ffab3b85c8f4f644c
Dmytro Fedoriaka <fedimser@gmail.com>
2025-07-30 22:28:04 -0700

    central state in create_graph

--- cayleypy/create_graph.py
+++ cayleypy/create_graph.py
@@ -8,6 +8,7 @@ def create_graph(**kwargs) -> CayleyGraph:
     Pass the following to kwargs:
         * "name" - the name of the graph, mandatory, see ``prepare_graph`` source for supported names,
         * other parameters of the graph (such as "n") that will be passed to ``prepare_graph``,
+        * "central_state" - central state, if you need to override it,
         * any arguments that are accepted by ``CayleyGraph`` constructor (e.g. ``verbose=2``).
 
     All passed kwargs will be first passed to ``prepare_graph`` to construct ``CayleyGraphDef`` and then to
@@ -19,4 +20,7 @@ def create_graph(**kwargs) -> CayleyGraph:
     This is not recommended in most cases. Instead, create ``CayleyGraphDef`` using one of library classes and then pass
     it to ``CayleyGraph`` constructor.
     """
-    return CayleyGraph(prepare_graph(**kwargs), **kwargs)
+    graph_def = prepare_graph(**kwargs)
+    if "central_state" in kwargs:
+        graph_def = graph_def.with_central_state(kwargs["central_state"])
+    return CayleyGraph(graph_def, **kwargs)
